344021a39ff48247358cf17bb44ace551ef1957b
maoyinshenzhihua <3811539889@qq.com>
2026-04-08 06:19:18 +0800

    Try build with IPHONEOS_DEPLOYMENT_TARGET

--- check_icon.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("e:\zmq\zmq\Assets.xcassets\AppIcon.appiconset\Icon.png")
-Write-Host "Width: $($img.Width)"
-Write-Host "Height: $($img.Height)"
-$img.Dispose()
