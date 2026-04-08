973e122060374f1e11f6febff5c283f99ae8ba14
maoyinshenzhihua <3811539889@qq.com>
2026-04-08 09:13:18 +0800

    Add DismissKeyboardTap component to Xcode project

--- zmq/UI/Pages/ConsoleView.swift
+++ zmq/UI/Pages/ConsoleView.swift
@@ -111,23 +111,3 @@ struct ConsoleView: View {
         }
     }
 }
-
-struct DismissKeyboardTap: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap))
-        tapGesture.cancelsTouchesInView = false
-        view.addGestureRecognizer(tapGesture)
-        return view
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
-    }
-
-    class Coordinator: NSObject {
-        @objc func handleTap() {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-        }
-    }
-}
--- zmq/UI/Pages/DeviceConnectView.swift
+++ zmq/UI/Pages/DeviceConnectView.swift
@@ -156,23 +156,3 @@ struct DismissKeyboardModifier: ViewModifier {
             .background(DismissKeyboardTap())
     }
 }
-
-struct DismissKeyboardTap: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap))
-        tapGesture.cancelsTouchesInView = false
-        view.addGestureRecognizer(tapGesture)
-        return view
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
-    }
-    
-    class Coordinator: NSObject {
-        @objc func handleTap() {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-        }
-    }
-}
